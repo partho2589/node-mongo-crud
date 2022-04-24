e9bd6506b2d9639fe4bed398d585b1116e6b085c
partho2589 <parthomalo615@.com>
2022-04-25 00:50:17 +0600

    added mongodb save data

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Running My node CRUD Server') 
+// user:dbuser1
+// password:yFbJKJPhwnIUmvGe
+
+
+
+const uri = "mongodb+srv://dbuser1:yFbJKJPhwnIUmvGe@cluster0.amg0m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("foodExpress").collection("users");
+    console.log(' db connected')
+    // perform actions on the collection object
+    client.close();
 });
 
-app.listen(port , () =>{
+async function run () {
+    try{
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        const user = {name:'rakib',email:"rakib@gmail.com"}
+        const result = await  userCollection.insertOne(user);
+        console.log(`User inserted with id: ${result.insertedId}`)
+   }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running My node CRUD Server')
+});
+
+app.listen(port, () => {
     console.log('CRUD server is running')
 })
